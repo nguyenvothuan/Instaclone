a69d7b80770bd1a86b9ed4a73c4105b491031269
Nguyen Vo Thuan <vothuan464.646@gmail.com>
2023-01-10 06:45:37 +0700

    Initial Commit

---
+++ Instaclone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instaclone
+//
+//  Created by Nguyen Vo Thuan on 1/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
